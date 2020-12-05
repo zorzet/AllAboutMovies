93b93c7e70d4c9c902081730ede9de357d242593
zorzet <44205109+zorzet@users.noreply.github.com>
2020-12-05 18:17:55 +0200

    Revert "Delete this change"
    This reverts commit 25bebe5f8402e03c52c52731026ace9d09a510fc.

--- AllAboutMovies/src/org/aueb/servlet/movies.java
+++ AllAboutMovies/src/org/aueb/servlet/movies.java
@@ -26,9 +26,7 @@ public class movies extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//this is a test line
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		response.notify();//to delete this line
 	}
 
 }
